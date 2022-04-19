3e3f5bc8a4fe11b39e8bc152c2704b61f412c81d
sgood15 <103536679+sgood15@users.noreply.github.com>
2022-04-19 17:53:00 -0400

    Update learn_steps.js
    Add next and back buttons

--- static/learn_steps.js
+++ static/learn_steps.js
@@ -30,8 +30,51 @@ function display_step(step, page, materials, tools){
         let to_append="<div class='col'><img src='"+value+"' alt='"+alt+"' class='img img-fluid'></img></div>"
         $("#visuals").append(to_append)
     })
+    let nextbutton = '<button class="next">Next</button>'
+    $('#nextbutton').append(nextbutton)
+    let backbutton = '<button class="back">Back</button>'
+    $('#backbutton').append(backbutton)
+}
+
+function next(page, id){
+    let pagenum = page
+    if(pagenum<3){
+        let nextpage = parseInt(page)
+        nextpage = nextpage+1
+        window.location.href="/learn/"+id+"/steps/"+nextpage
+    }
+    else{
+        if(id==2){
+            window.location.href="/quiz/0"
+        }
+        else{
+            let nextid = parseInt(id)
+            nextid = nextid+1
+            window.location.href="/learn/"+nextid+"/materials/1"
+        }
+    }
+    
+}
+function back(page, id){
+    if(page==1){
+        window.location.href="/learn/"+id+"/materials/2"
+    }
+    else{
+        let prevpage = parseInt(page)
+        prevpage = prevpage-1
+        window.location.href="/learn/"+id+"/steps/"+prevpage
+    }
+    
 }
 
 $(document).ready(function(){
     display_step(step, page, materials, tools)
+    
+    $(".next").click(function(){
+        next(page, id)
+    })
+
+    $(".back").click(function(){
+        back(page, id)
+    })
 })
