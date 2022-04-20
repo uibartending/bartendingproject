aa5875566f69cfb2176af47c3e252b54a660a9e5
wugun <56032867+wugun@users.noreply.github.com>
2022-04-20 18:25:04 -0400

    Update home.js

--- static/home.js
+++ static/home.js
@@ -6,20 +6,23 @@ function load_home(data){
     })
 }
 
+function get_entry_time(user_data) {
+  var today = new Date();
+  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var dateTime = date+' '+time;
+  console.log(dateTime);
+  user_data["loginTime"] = dateTime
+  console.log(user_data)
+}
+
 function startlearn(){
     window.location.href="/learn/0/materials/1"
 }
 
 $(document).ready(function(){
     load_home(data)
-    
-    //get user's login time
-    var today = new Date();
-    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    var dateTime = date+' '+time;
-    console.log(dateTime);
-
+    get_entry_time(user_data)
     $("#startbutton").click(function(){
         startlearn()
     })
