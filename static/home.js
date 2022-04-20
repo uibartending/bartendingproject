432017c405c08bf8df0d58cdc072e2f5825320fa
wugun <56032867+wugun@users.noreply.github.com>
2022-04-20 11:35:47 -0400

    Update home.js with user

--- static/home.js
+++ static/home.js
@@ -12,6 +12,13 @@ function startlearn(){
 
 $(document).ready(function(){
     load_home(data)
+    
+    //get user's login time
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date+' '+time;
+    console.log(dateTime);
 
     $("#startbutton").click(function(){
         startlearn()
